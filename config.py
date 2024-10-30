e640d8e6f6f23c678b9942bddc67352390f0567c
ruben hurtado <rubenmau@unicauca.edu.co>
2024-10-30 16:05:14 -0500

    ss

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://sis_4n7m_user:9YCtxXMrAOOvhTulWZNEA36mbe2wyGl7@dpg-csh84arv2p9s738q7gq0-a.oregon-postgres.render.com/sis_4n7m'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://sisdatabase_user:j3EmwpWH1Kxr9e9Jn0e2IoqCGlEJW6eH@dpg-csh9rqo8fa8c73eficu0-a.oregon-postgres.render.com/sisdatabase'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
