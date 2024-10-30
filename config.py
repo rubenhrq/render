a03578db300ae592c8ee7349a7ccb38b848f5ff1
rubenhrq <147635447+rubenhrq@users.noreply.github.com>
2024-10-30 15:09:11 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://testdb_fwms_user:MclrHbdtGttpwx7NvXqN286mtg4LfEgD@dpg-crue83jv2p9s73epvrf0-a/testdb_fwms'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://sis_4n7m_user:9YCtxXMrAOOvhTulWZNEA36mbe2wyGl7@dpg-csh84arv2p9s738q7gq0-a.oregon-postgres.render.com/sis_4n7m'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
